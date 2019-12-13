4c02c8e6458ef85a395f73d45c700478ae3ea160
James Harton <james@automat.nz>
2019-12-13 13:19:26 +1300

    Bump to 0.2.2.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PCA9641.MixProject do
   def project do
     [
       app: :pca9641,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: "Driver for PCA9641 2-channel I2C bus master arbiter chip",
