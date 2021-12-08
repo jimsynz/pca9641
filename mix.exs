f7c468e9e076df63d7b55da4efd0600d30a8c384
James Harton <james@harton.nz>
2021-12-09 12:45:50 +1300

    chore(deps): update dependency credo to ~> 1.6

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule PCA9641.MixProject do
     [
       {:circuits_gpio, "~> 1.0", optional: true},
       {:circuits_i2c, "~> 1.0", optional: true},
-      {:credo, "~> 1.1", only: ~w[dev test]a, runtime: false},
+      {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
       {:earmark, ">= 0.0.0", only: ~w[dev test]a},
       {:elixir_ale, "~> 1.2", optional: true},
       {:ex_doc, ">= 0.0.0", only: ~w[dev test]a},
