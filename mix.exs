e84c023a18c0bcd15a708dafa37a0870b4cd995f
James Harton <james@harton.nz>
2021-12-09 15:56:00 +1300

    chore(deps): update dependency ex_doc to >= 0.26.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule PCA9641.MixProject do
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
       {:earmark, ">= 1.4.19", only: ~w[dev test]a},
       {:elixir_ale, "~> 1.2", optional: true},
-      {:ex_doc, ">= 0.0.0", only: ~w[dev test]a},
+      {:ex_doc, ">= 0.26.0", only: ~w[dev test]a},
       {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false},
       {:mimic, "~> 1.1", only: :test},
       {:wafer, "~> 0.3"}
